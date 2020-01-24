bf8a9b831df9b3db1c79f4120229944413462b5f
Jessica.E.Spencer <jessicaspencer12@gmail.com>
2020-01-24 15:57:22 -0500

    Added functionality for misspelled user input

--- dictapp.py
+++ dictapp.py
@@ -1,6 +1,8 @@
 #python3
-# Program that loads json dictionary of items in
+# Program that loads json dictionary of items in and adjusts for mispelled words
 
+import difflib
+from difflib import get_close_matches
 import os
 import json
 
@@ -10,4 +12,26 @@ filename = os.path.join(dirname, 'data.json')
 
 # Load data 
 data = json.load(open(filename,'r+'))
-print(data)
+
+# Function that inputs key and returns a value 
+def translate(word):
+    word = word.lower()
+    if word in data:
+        return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no." % get_close_matches(word, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
+
+    else:
+        return "The word does not exist please double check it." 
+
+
+word = input("Enter word: ")
+
+print(translate(word))
+
