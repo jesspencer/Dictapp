960050300ad6de1949e9ef09828cb724a523f1eb
Jessica.E.Spencer <jessicaspencer12@gmail.com>
2020-01-30 11:36:48 -0500

    Updated script to include title words

--- dictapp.py
+++ dictapp.py
@@ -15,15 +15,26 @@ data = json.load(open(filename,'r+'))
 
 # Function that inputs key and returns a value 
 def translate(word):
+    
     word = word.lower()
+    
     if word in data:
         return data[word]
+    
+    # Adding line to account for titles such as Paris  
+    elif word.title() in data:
+        return data[word.title()]
+    
     elif len(get_close_matches(word, data.keys())) > 0:
-        yn = input("Did you mean %s instead? Enter Y if yes, or N if no." % get_close_matches(word, data.keys())[0])
+    
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(word, data.keys())[0])
+        
         if yn == "Y":
             return data[get_close_matches(word, data.keys())[0]]
+        
         elif yn == "N":
             return "The word doesn't exist. Please double check it."
+        
         else:
             return "We didn't understand your entry."
 
