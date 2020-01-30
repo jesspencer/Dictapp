d20626da1085d82d175f03e72c799ec2078cf29d
Jessica.E.Spencer <jessicaspencer12@gmail.com>
2020-01-30 11:43:57 -0500

    Appended the script to account for acronyms

--- dictapp.py
+++ dictapp.py
@@ -25,6 +25,10 @@ def translate(word):
     elif word.title() in data:
         return data[word.title()]
     
+    # Adding line to account for words that are acronyms 
+    elif word.upper() in data:
+        return data[word.upper()]
+    
     elif len(get_close_matches(word, data.keys())) > 0:
     
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(word, data.keys())[0])
